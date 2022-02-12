ba91912efb3cd5cf08369dcd43f42702d15d88b8
srach3 <90348030+srach3@users.noreply.github.com>
2022-02-12 14:05:42 -0800

    Initial Commit

---
+++ lOKA/lOKAApp.swift
@@ -0,0 +1,17 @@
+//
+//  lOKAApp.swift
+//  lOKA
+//
+//  Created by Samhita Rachapudi on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct lOKAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lOKAUITests/lOKAUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lOKAUITestsLaunchTests.swift
+//  lOKAUITests
+//
+//  Created by Samhita Rachapudi on 2/12/22.
+//
+
+import XCTest
+
+class lOKAUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
